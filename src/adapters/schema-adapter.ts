e9a7eab5abaad2d5299ea82d3b0671e0d2cdda0a
Spencer Cain <cainspencerm@protonmail.com>
2025-07-03 19:49:22 -0400

    feat: modify SchemaAdapter to have only an optional parse method

--- src/adapters/schema-adapter.ts
+++ src/adapters/schema-adapter.ts
@@ -1,6 +1,3 @@
 export interface SchemaAdapter<T> {
-  parse(env: NodeJS.ProcessEnv): T
-  safeParse(
-    env: NodeJS.ProcessEnv
-  ): { success: true; data: T } | { success: false; error: unknown }
+  parse?: (env: NodeJS.ProcessEnv) => T
 }
