19214d33bbb5822321e689a1fece532b1ac742c1
Spencer Cain <cainspencerm@protonmail.com>
2025-07-04 02:21:43 -0400

    feat: add support for joi's validate schema function in Schema

--- src/schemas/schema.ts
+++ src/schemas/schema.ts
@@ -1,8 +1,9 @@
 export type Schema<T> = {
   parse?: (env: NodeJS.ProcessEnv) => T
+  validate?: (env: NodeJS.ProcessEnv) => { value: T; error?: Error }
 }
 
-const schemaParsingFunctions = ["parse"] as const
+const schemaParsingFunctions = ["parse", "validate"] as const
 
 export type SchemaParserName = (typeof schemaParsingFunctions)[number]
 
@@ -10,8 +11,18 @@ function zodParser<T>(env: NodeJS.ProcessEnv, schema: Schema<T>): T {
   return schema.parse!(env)
 }
 
+function joiParser<T>(env: NodeJS.ProcessEnv, schema: Schema<T>): T {
+  const result = schema.validate!(env)
+  if (result.error) {
+    throw result.error
+  }
+
+  return result.value as T
+}
+
 export type SchemaParser = <T>(env: NodeJS.ProcessEnv, schema: Schema<T>) => T
 
 export const schemaParsers: Record<SchemaParserName, SchemaParser> = {
   parse: zodParser,
+  validate: joiParser,
 }
