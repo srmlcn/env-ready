8613c2b77349f0e809568192047e715529ee4ef1
Spencer Cain <cainspencerm@protonmail.com>
2025-07-04 01:51:54 -0400

    refactor: modularize schema parsing for validation logic isolation

--- src/schemas/schema.ts
+++ src/schemas/schema.ts
@@ -1,5 +1,17 @@
-export interface Schema<T> {
+export type Schema<T> = {
   parse?: (env: NodeJS.ProcessEnv) => T
 }
 
-export const possibleSchemaFunctions = ["parse"] as const
+const schemaParsingFunctions = ["parse"] as const
+
+export type SchemaParserName = (typeof schemaParsingFunctions)[number]
+
+function zodParser<T>(env: NodeJS.ProcessEnv, schema: Schema<T>): T {
+  return schema.parse!(env)
+}
+
+export type SchemaParser = <T>(env: NodeJS.ProcessEnv, schema: Schema<T>) => T
+
+export const schemaParsers: Record<SchemaParserName, SchemaParser> = {
+  parse: zodParser,
+}
