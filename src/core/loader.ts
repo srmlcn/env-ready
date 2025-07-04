ea0b9ed648a91db3054741f556ac19ab8a96c6d2
Spencer Cain <cainspencerm@protonmail.com>
2025-07-03 20:11:47 -0400

    feat: search for the available schema parsing function in loadEnv

--- src/core/loader.ts
+++ src/core/loader.ts
@@ -1,13 +1,30 @@
 import { InvalidEnvironmentError } from "@/errors/invalid-environment-error"
-import type { Schema } from "@/schemas/schema"
+import { InvalidSchemaError } from "@/errors/invalid-schema-error"
+import { ensureError } from "@/errors/utils"
+import { possibleSchemaFunctions, type Schema } from "@/schemas/schema"
 
-export function loadEnv<T>(adapter: Schema<T>): T {
-  const result = adapter.safeParse(process.env)
-  if (!result.success) {
-    throw new InvalidEnvironmentError(
-      "Invalid environment variables",
-      result.error
-    )
+export function loadEnv<T>(schema: Schema<T>): T {
+  let parsed: T
+
+  for (const funcName of possibleSchemaFunctions) {
+    if (schema[funcName] === undefined) continue
+
+    try {
+      parsed = schema[funcName](process.env)
+    } catch (_error) {
+      const error = ensureError(_error)
+      throw new InvalidEnvironmentError(
+        "Failed to parse environment variables",
+        error
+      )
+    }
+
+    return parsed
   }
-  return result.data
+
+  throw new InvalidSchemaError(
+    `Schema must implement one of the following methods: ${possibleSchemaFunctions.join(
+      ", "
+    )}`
+  )
 }
